3d75c1d917be6951935e3777aead2b78041a1d0b
NachoCarvajal <nachoch64@correo.ugr.es>
2023-03-03 09:27:38 +0100

    Add style for left part of the component

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,48 @@ import './App.css'
 function App() {
 
   return (
-    <div className="App">
-        <h1>React App</h1>        
-    </div>
+    <div className="app__container">
+        <div className='app__container_left'>    
+          <div className='app__container_left_title'>
+            <h2>Your Result</h2>
+          </div>
+          <div className='app__container_left_score'>
+            <div className='app__container_left_score_user'>76</div>
+            <div className='app__container_left_score_total'>of 100</div>
+          </div>
+          <div className='app__container_left_bottom'>
+            <div className='app__container_left_bottom_result'>Great</div>
+            <div className='app__container_left_bottom_compare'>
+              You scored higher than 65% of the people who have taken these tests.
+            </div>
+          </div>
+        </div>
+
+        <div className='app__container_right'>
+          <div className='app__container_right_title'>
+            <h2>Summary</h2>
+          </div>
+          <div className='app__container_right_summary'>
+            <div className='app__container_right_summary_item'>
+              <h3>Field</h3>
+              <p>Value</p>
+            </div>
+            <div className='app__container_right_summary_item'>
+              <h3>Field</h3>
+              <p>Value</p>
+            </div>
+            <div className='app__container_right_summary_item'>
+              <h3>Field</h3>
+              <p>Value</p>
+            </div>
+            <div className='app__container_right_summary_item'>
+              <h3>Field</h3>
+              <p>Value</p>
+            </div>
+            <button className='app__container_right_button'>Continue</button>
+          </div>
+        </div>
+      </div>
   )
 }
 
